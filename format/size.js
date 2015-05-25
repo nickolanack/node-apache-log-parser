b8feda8f09ce7c6f3a306ffff15972d173543f35
Nick <nickblackwell@gmail.com>
2015-05-25 14:50:39 -0700

    initial

---
+++ format/size.js
@@ -0,0 +1,31 @@
+module.exports = {
+                format: format
+}
+function format(data){
+	
+	
+	var bytes=data.bytes;
+	var units='b';
+	if(bytes>1024){
+		bytes=bytes/1024.0;
+		units='kb';
+	}
+
+	if(bytes>1024){
+		bytes=bytes/1024.0;
+                                units='mb';
+	}
+
+	if(bytes>1024){
+                                bytes=bytes/1024.0;
+                                units='gb';
+                        }
+	
+	if(units!=='b'){
+		bytes=Math.round(bytes*10)/10.0;
+	}
+	
+	return {bytes:bytes+units};
+	
+	
+}
